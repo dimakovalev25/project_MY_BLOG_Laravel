4b12d6d936849876167790a0f11f29c6b762e459
dimakovalev <d.nozdryn@gmail.com>
2023-05-06 14:08:28 +0300

    first controller

---
+++ app/Http/Main/IndexController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Main;
+
+use App\Http\Controllers\Controller;
+
+class IndexController extends Controller
+{
+    public function __invoke()
+    {
+    return 'bbb';
+    }
+}
